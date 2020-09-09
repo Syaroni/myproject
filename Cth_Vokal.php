bc1147f826ed80e6b642d2dd9ca1aaa5deaf2a1b
Syaroni <53205259+Syaroni@users.noreply.github.com>
2020-09-09 16:26:44 +0700

    Add files via upload
    Belum Selesai

---
+++ Cth_Vokal.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<input type="text" id="idtext" value="">
+<button onclick="myFunction()">Try it</button>
+<p id="demo"></p>
+<script>
+function myFunction() {
+  var x = document.getElementById("idtext").value;
+  var res = str.replace("a", "");
+  //document.getElementById("demo").innerHTML = x;
+  document.write(res);
+}
+</script>
+</body>
+</html>
